fad5ed3081d48c21108dc6a37c6dd1a89252f6ba
Dan <danbarry16@googlemail.com>
2020-02-08 00:12:59 +1300

    Basic test source file

---
+++ src/test.c
@@ -0,0 +1,17 @@
+#include "pt-draw.c"
+
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+int total;
+int pass;
+int fail;
+
+int main(){
+  /* Setup global variables */
+  total = pass = fail = 0;
+  /* Test was successful */
+  return EXIT_SUCCESS;
+}
