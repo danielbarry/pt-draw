81f6a7ec36fd438d807166407c7f30bb6bf0764f
Dan <danbarry16@googlemail.com>
2020-02-08 00:35:05 +1300

    Basic test framework

--- src/test.c
+++ src/test.c
@@ -2,16 +2,24 @@
 
 #include <stdio.h>
 
+#define FALSE 0
+#define TRUE 1
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+#define ASSERT(x,s) {x ? ++pass : ++fail; printf("  %s %s::%i %s\n", (x ? "[ OK ]  " : "  [FAIL]"), __FUNCTION__, __LINE__, s);}
 
-int total;
 int pass;
 int fail;
 
 int main(){
   /* Setup global variables */
-  total = pass = fail = 0;
+  pass = fail = 0;
+  /* Perform tests */
+  ASSERT(TRUE, "test");
+  ASSERT(FALSE, "hello");
+  /* Print result */
+  printf("%i / %i PASS\n", pass, pass + fail);
+  printf("%i / %i FAIL\n", fail, pass + fail);
   /* Test was successful */
-  return EXIT_SUCCESS;
+  return fail <= 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
