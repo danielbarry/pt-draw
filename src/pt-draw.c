5a294c12f89d86fb1db90b032be1f51f5b1b5c5e
Dan <danbarry16@googlemail.com>
2020-02-09 06:06:47 +1300

    Implementation of quadratic curve

--- src/pt-draw.c
+++ src/pt-draw.c
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 
+#define GET_POINT(i,j,p) ( i + ((j - i) * p) )
+
 void pt_draw_simple_line(UINT8 xy1, UINT8 xy2, UINT8* buff, unsigned int size){
   int x1, y1, x2, y2, dx, dy, sx, sy, err, e2;
   /* Get line variables */
@@ -53,6 +55,52 @@ void pt_draw_simple_line(UINT8 xy1, UINT8 xy2, UINT8* buff, unsigned int size){
   }
 }
 
+void pt_draw_curved_line(UINT8 xy1, UINT8 xy2, UINT8 cxy, UINT8* buff, unsigned int size){
+  int x1, y1, x2, y2, cx, cy, xa, ya, xb, yb, x, y;
+  /* Get line variables */
+  x1 = (xy1 & 0b11110000) >> 4;
+  y1 = xy1 & 0b1111;
+  x2 = (xy2 & 0b11110000) >> 4;
+  y2 = xy2 & 0b1111;
+  cx = (cxy & 0b11110000) >> 4;
+  cy = cxy & 0b1111;
+  printf("(%i,%i) (%i,%i) (%i,%i)", x1, y1, x2, y2, cx, cy); // TODO
+  /* Scale the points up if needed */
+  if(size > 15){
+    int scale = size / 15;
+    x1 = x1 * scale;
+    y1 = y1 * scale;
+    x2 = x2 * scale;
+    y2 = y2 * scale;
+    cx = cx * scale;
+    cy = cy * scale;
+  }
+  /* Scale the points down if needed */
+  if(size < 15){
+    int scale = 15 / size;
+    x1 = x1 / scale;
+    y1 = y1 / scale;
+    x2 = x2 / scale;
+    y2 = y2 / scale;
+    cx = cx / scale;
+    cy = cy / scale;
+  }
+  /* Draw quadratic curve */
+  for(float i = 0; i < 1; i += 0.01f){
+    /* Support points */
+    xa = GET_POINT(x1, cx, i);
+    ya = GET_POINT(y1, cy, i);
+    xb = GET_POINT(cx, x2, i);
+    yb = GET_POINT(cy, y2, i);
+    /* Draw points */
+    x = GET_POINT(xa, xb, i);
+    y = GET_POINT(ya, yb, i);
+    /* Set pixel */
+    int p = x + (y * size);
+    buff[p / 8] |= 0b1 << (7 - (p % 8));
+  }
+}
+
 void pt_draw_create_bitmap(UINT8* desc, unsigned int len, UINT8* buff, unsigned int size){
   /* TODO: Basic sanity checks before starting. */
   /* Loop over the description */
@@ -67,7 +115,8 @@ void pt_draw_create_bitmap(UINT8* desc, unsigned int len, UINT8* buff, unsigned 
         z += 2;
         break;
       case LIN_CURV:
-        /* TODO: Process simple curve. */
+        pt_draw_curved_line(desc[z + 1], desc[z + 2], desc[z + 3], buff, size);
+        z += 3;
         break;
       case SEQ_SIMP:
         /* TODO: Handle fill condition. */
@@ -78,6 +127,7 @@ void pt_draw_create_bitmap(UINT8* desc, unsigned int len, UINT8* buff, unsigned 
         break;
       case SEQ_CURV:
         /* TODO: Process sequenced curve. */
+        /* TODO: Handle fill condition. */
         break;
     }
   }
--- src/pt-draw.h
+++ src/pt-draw.h
@@ -34,6 +34,22 @@
   void pt_draw_simple_line(UINT8 xy1, UINT8 xy2, UINT8* buff, unsigned int size);
 
   /**
+   * pt_draw_curved_line()
+   *
+   * Draw a quadratic curved line to the binary bitmap from the given parameters.
+   *
+   * @param xy1 The (X1,Y1) pair in a single byte.
+   * @param xy2 The (X2,Y2) pair in a single byte.
+   * @param cxy The (CX,CX) pair in a single byte.
+   * @param buff A pointer to the binary buffer to be set. The buffer must have
+   * adequate space for storing the resulting data.
+   * @param size The size of the width and height (size = width = height). The
+   * number of pixels in total is size x size, total number of bytes is (size x
+   * size) / 8 rounded up to the nearest byte.
+   **/
+  void pt_draw_curved_line(UINT8 xy1, UINT8 xy2, UINT8 cxy, UINT8* buff, unsigned int size);
+
+  /**
    * pt_draw_create_bitmap()
    *
    * This method is used for creating a binary bitmap from the scalable vector
