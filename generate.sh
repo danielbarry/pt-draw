a175ccc58e8fe2b8e5f131dd76ecb7260ca4ac3b
Dan <danbarry16@googlemail.com>
2020-02-14 15:44:53 +1300

    Generate available font letters

--- generate.sh
+++ generate.sh
@@ -7,6 +7,12 @@ make
 # Run test program
 ./pt-draw-test
 
+# Convert the letter files from PPM to PNG
+for f in font-*.ppm; do
+  file="${f%.*}"
+  convert "$f" "doc/$file.png"
+done
+
 # Convert the icons files from PPM to PNG
 for f in icon-*.ppm; do
   file="${f%.*}"
